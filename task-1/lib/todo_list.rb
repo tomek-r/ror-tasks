b7aa2790cf523c07649a178a7d2446924f25515d
Tomasz Rejment <08_rejment@wierzba.wzks.uj.edu.pl>
2013-03-07 14:00:17 +0100

    simple implementation of todolist class

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,31 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    if items.nil?
+        raise IllegalArgument
+    end
+    @list = items
+    @completed = Hash.new(false)
+  end
+  def empty?
+    @list.empty?
+  end
+  def size
+    @list.size
+  end
+  def << (item)
+    @list << item
+  end
+  def last
+    @list.last
+  end
+  def completed? (index)
+    @completed[index]
+  end
+  def first
+    @list.first
+  end
+  def complete (index)
+    @completed[index] = true
   end
 end
