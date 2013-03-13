9f8cc9a5f552f2c45d05dd0b5c3bc218b0277d09
Tomek <trejment@gmail.com>
2013-03-13 17:49:25 +0100

    item class indentation

--- task-1/lib/todo_item.rb
+++ task-1/lib/todo_item.rb
@@ -2,8 +2,8 @@ class Item
   attr_accessor :completed, :description
 
   def initialize (description = '')
-	  @description = description
-	  @completed = false
+    @description = description
+    @completed = false
   end
   
   def to_s
